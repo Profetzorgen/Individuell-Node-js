5e83a2a69d4ce1d5a144f35502881894fe94c3af
Profetzorgen <matti.heinonen1988@gmail.com>
2021-12-04 14:52:09 +0100

    Gjorde om allt med fungerande exempel, form input

--- indexmh.js
+++ indexmh.js
@@ -1,69 +1,28 @@
-// mattis individuella inlämning. Version 1.0
-// Denna är under utveckling och fungerar inte riktigt rätt ännu.
-// Beskrivande inledande rubrik, sedan inmatningsfält med:
-// 1.tids-stämpel
-// 2.namn (senare dolt namn, valt användarnamn)
-// 3.email
-// 4.telefonnummer
-// 5.inlägg
-// Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-// get element...
 
-const express = require("express");
+
+const express = require("express")
+const path = require("path");
+  
 const app = express();
-///////////////////////////
-let fs =require("fs");
-const readline = require('readline');
-const r1 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-app.use(express.static("publikmapp"));
+const port = process.env.PORT || 3000;
+  
+// Setting path for public directory 
+const static_path = path.join(__dirname, "publikmapp");
+app.use(express.static(static_path));
 app.use(express.urlencoded({ extended: true }));
-//////////////////////////
+  
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/indexmhh.html");
+    res.sendFile(__dirname + "/publikmapp/indexmhh.html");
 });
-app.listen(3000);
-console.log("Gästboken är online.");
-
-
-app.get("/processa-forfragan", (req, res) => {
-    
-    console.log("Mottog förfrågan från klient xhttp");
-
-    res.send("Server säger: du lyckades!");
-    jsonHelaRasket();
-});
-let jsonHelaRasket = function () {
-    let data = `
-    [{
-    "namn": "Sigbjörn Brunto",
-    "email": "sbjornB@dunderkloss.se",
-    "mobil": "0701234567",
-    "inlagg": "Är det här man skriver in något och trycker på skicka?",
-    "pass": "Lösenord1234"
-    }, {
-    "namn": "Skrytbengt MacFjärdhundra",
-    "email": "bestbengan@bengtarbast.bengt",
-    "mobil": "0761234567",
-    "inlagg": "Ja visst fan är det här du trycker skicka, imbecill!",
-    "pass": "IngenHybris1337"
-    }]
-    `;
-        let object = JSON.parse(data);
-        console.log("First object name: " + object[0].name);
-        fs.writeFile("km10.json", JSON.stringify(object), function(err){
-            if(err) throw err;
-            console.log('Ny fil sparad, om det inte fanns någon');
-        });
-        //// första delen ovanför, andra eller tredje här under
-    fs.readFile("km10.json", function(err,data){
-    let json = JSON.parse(data);
-    json.push({"namn": namn, "email": email, "mobil": mobil, "inlagg": inlagg, "pass": pass});
-    fs.writeFile("km10.json", JSON.stringify(json), function(err){
-        if(err) throw err;
-        console.log("Om detta fungerade, har du nu sparat informationen korrekt!");
-    });
+// Handling request 
+app.post("/request", (req, res) => {
+   res.json([{
+      name_recieved: req.body.name,
+      designation_recieved: req.body.designation
+   }])
 })
-}
+  
+// Server Setup
+app.listen(port, () => {
+   console.log(`server is running at ${port}`);
+});
--- publikmapp/klientmh.js
+++ publikmapp/klientmh.js
@@ -1,22 +1,13 @@
-const { query, urlencoded } = require("express");
-const express = require("express");
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-// Detta är klientskript, den ska i stort sett bara validera input, spara den, skicka den med ajax.
-window.onload = function() {
-    let namn = document.getElementById('namn');
-    let email = document.getElementById('email');
-    let mobil = document.getElementById('mobil');
-    let inlagg = document.getElementById('gestboksinlegg');
-    let pass = document.getElementById('pass'); // .value?
-app.post("/processa-forfragan", (req, res) =>{
-    namn = String(req.body.namn);
-    email = String(req.body.email);
-    mobil = String(req.body.mobil);
-    inlagg = String(req.body.inlagg);
-    pass = String(req.body.pass);
-    res.send(`${namn}${email}${mobil}${inlagg}${pass}`);
-})
-}
-// document.getElementById("knapp").innerHTML = this.response;
 
+$(document).ready(function () {
+    $("#submit").click(function () {
+       $.post("/request",
+          {
+             name: "viSion",
+             designation: "Professional gamer"
+          },
+          function (data, status) {
+             console.log(data);
+          });
+    });
+ });
