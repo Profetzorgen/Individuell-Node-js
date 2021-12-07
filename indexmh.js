87badd14c20de176210d770a93fe1a43ec36a768
Profetzorgen <matti.heinonen1988@gmail.com>
2021-12-07 09:38:46 +0100

    gjorde om html inputfälten till rätt typer

--- indexmh.js
+++ indexmh.js
@@ -1,28 +1,66 @@
-
-
-const express = require("express")
-const path = require("path");
+// Tydligen räcker den här mängden kod
+const express = require("express") // const = går ej att ändra,spikad variabel
+const path = require("path"); // express vet ni vad det är, en grej i javascript.
   
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express(); // app använder express
+const port = process.env.PORT || 3000; // såhär skriver man tydligen också. säger bara 
+// lyssna på 3000.
   
-// Setting path for public directory 
+// Kopierad kod från nätet,, blev så rörigt att klippa och klistra.
+// denna under ska iaf dra ihop projektmappen med diverse annat.
 const static_path = path.join(__dirname, "publikmapp");
 app.use(express.static(static_path));
 app.use(express.urlencoded({ extended: true }));
-  
+//...men om den inte gör det, se under:
+
+// Eftersom jag lagt mina mappar som en möglig kråka behöver jag ha denna här under:
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/publikmapp/indexmhh.html");
 });
-// Handling request 
-app.post("/request", (req, res) => {
-   res.json([{
-      name_recieved: req.body.name,
-      designation_recieved: req.body.designation
-   }])
-})
-  
-// Server Setup
+// Oavsett vad så måste man lägga rätt sökväg tills allt hittar varann.. mer om det
+// senare...
+
+
+// Det klonkade till i port 3000! är det ett fyllo? är det en inbrottstjuv? NEJ!
+// För det är bara ett inkommande fanskap som vill åt /request!
+// Och om dom vill ha ett svar, då är det bäst att de ger mig 5 variabler i json-format!
+app.post("/request", (req, res) => { // När någon postar mig strängvariabler upplagda som i json
+   res.json([{ // spara dem i json-format.
+      namn_recieved: req.body.namn, // Jag fick ett namn, för jag requestade ett namn.
+      mobil_recieved: req.body.mobil,
+      email_recieved: req.body.email,
+      inlagg_recieved: req.body.inlagg,
+      pass_recieved: req.body.pass,
+   }]) // ALLT FUNKADE, ALLA VARIABLER STÄMMER!ja men..
+});//<----- Ta en kringla vetja! Var inte blyg, det finns massor!
+
+
+// Jag är server, en kort klase med kod, jag lyssnar på port 3000 och svarar
+// på klienter (användarjäklarnas) anrop.
+// <----- det syns här till vänster att nodemon startar om servern varje gång jag skrivit
+// en endaste bokstav.
 app.listen(port, () => {
    console.log(`server is running at ${port}`);
 });
+
+/* 
+skriva till fil, läsa från fil
+*/
+
+///
+      /// ANTECKNINGAR, ATT GÖRA: Skriva fil, uppdatera hemsida, ajax!
+      /*
+      1.Ha möte, lär teamet hur man skriver till fil!
+      2.Kolla om nåt annat ska läras.
+      3.Kolla om min kod behöver ändras, förklaras.
+      4.När mötet är klart: 
+       --skriva till fil och läsa från fil på individuella
+       --uppdatera hemsidan med ajax om nån gör ett inlägg utan att ladda om sidan
+       --jquery dropdown,menynavigering,inloggningskonto-hantering (async?)
+       --validering serverskript, sedan, validering klientskript
+       --skriva till fil och läsa till fil på gemensamma
+       --uppdatera hemsidan, gemensamma
+       --slutligen=styling med css på både gemensama och individuella uppg.
+       --uppgifter del 2. sockets etc.
+      */
+///
--- publikmapp/klientmh.js
+++ publikmapp/klientmh.js
@@ -1,13 +1,43 @@
+// SÅHÄR BLEV DET FÄRDIGA RESULTATET:
+$(document).ready(function () { // samma som window.onload = function
+    $("#submit").click(function () { // vid klick på knappen, skicka.
+        $.post("/request", // post-request, obs här i klientskript.
+           {// Användaren fyller i på sidan,trycker submit lr vad det står.
+            // Det här skriptet gör om innehållet i formulär-rutorna till textsträng i json-format även fast det inte ser så ut..
+            //..men document.getElementById('namn').value är samma sak som "Svalbard Målvaktsson" i vårt exempel...
+              namn: document.getElementById('namn').value, //..istället för exempel-strängen name: "viSion",
+              mobil: document.getElementById('mobil').value,// obs alla inputs i html är av type="text" än så länge, ska ändras.
+              email: document.getElementById('email').value,// denna ska va type="email"
+              inlagg: document.getElementById('inlagg').value,// denna är textarea men de andra är type= text
+              pass: document.getElementById('pass').value// denna är pass, // kom ihåg "pass.on(blur)"" vid behov vid validering.
+           },
+           function (data, status) { // Här sker någon form av level 8 svartkonst. Ska be Macke ringa och fråga åklagarn.
+              console.log(data); // men den skriver iaf ut data i das consöl.
+           });
+           
+           // Här är en fiffig grej som underlättar förståelsen hoppas jag, skriver ut i konsol vad det är för typ av...
+           // ...input man orsakat, notera tex Inläggvariabeln="textarea" i konsölla.
+           console.log("Namnvariabel: " + namn +
+           "\nMobilvariabel: "+ mobil + 
+           "\nEmailvariabel: "+ email + 
+           "\nInläggvariabel: " + inlagg +
+           "\nPasswordvariabel: " +pass);
+           // Detta console.log skrev även ut exakt inmatning när man hade document.getElementByID('namn tex')...
+           //...men för att koden ska bli så kort som möjligt så är rubbet gröntext.
+     });
+});// Älskar alla alzheimers-krumelurer som dyker upp överallt...
 
-$(document).ready(function () {
-    $("#submit").click(function () {
-       $.post("/request",
-          {
-             name: "viSion",
-             designation: "Professional gamer"
-          },
-          function (data, status) {
-             console.log(data);
-          });
-    });
- });
+// DET HÄR KOPIERADE JAG FRÅN NÄTET:
+//  $(document).ready(function () {
+//     $("#submit").click(function () {
+//        $.post("/request",
+//           {
+//              name: "viSion",
+//              designation: "Professional gamer"
+//           },
+//           function (data, status) {
+//              console.log(data);
+//           });
+//     });
+//  });name: 
+// Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
