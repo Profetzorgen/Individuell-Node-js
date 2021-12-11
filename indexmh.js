c957c7dcdaf8bb2c6ee8cbf3fc6da88417388479
Profetzorgen <matti.heinonen1988@gmail.com>
2021-12-11 21:09:43 +0100

    justerade json hantering på serv.

--- indexmh.js
+++ indexmh.js
@@ -1,53 +1,62 @@
-// Tydligen räcker den här mängden kod
-const express = require("express") // const = går ej att ändra,spikad variabel
-const path = require("path"); // express vet ni vad det är, en grej i javascript.
-// const { body, validationResult } = require('express-validator');
-// var validator = require('validator');
-// validator.isEmail('foo@bar.com');
-// import validator from 'validator'; 
-
-const app = express(); // app använder express
-const port = process.env.PORT || 3000; // såhär skriver man tydligen också. säger bara 
-// lyssna på 3000.
-  
-// Kopierad kod från nätet,, blev så rörigt att klippa och klistra.
-// denna under ska iaf dra ihop projektmappen med diverse annat.
-const static_path = path.join(__dirname, "publikmapp");
+const express = require("express");
+const fs = require("fs");
+const path = require("path"); 
+const { body, validationResult } = require("express-validator");
+const port = process.env.PORT || 3000;
+const app = express();
+// Det är här som path drar ihop projektmappen med diverse annat på något vis.
+app.use(express.static(path.join(__dirname, "publikmapp")));
 app.use(express.static(static_path));
-app.use(express.urlencoded({ extended: true }));
-//...men om den inte gör det, se under:
+// Ger express tillgång till input från formulär. Vet ej vad extended: true/false gör.
+app.use(express.urlencoded({ extended: false }));
+
+let omvandlaText = new Array();
 
 // Eftersom jag lagt mina mappar som en möglig kråka behöver jag ha denna här under:
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/publikmapp/indexmhh.html");
 });
-// Oavsett vad så måste man lägga rätt sökväg tills allt hittar varann.. mer om det
-// senare...
 
 
-// Det klonkade till i port 3000! är det ett fyllo? är det en inbrottstjuv? NEJ!
-// För det är bara ett inkommande fanskap som vill åt /request!
-// Och om dom vill ha ett svar, då är det bäst att de ger mig 5 variabler i json-format!
-app.post("/request", (req, res) => { // När någon postar mig strängvariabler upplagda som i json
-   res.json([{ // spara dem i json-format.
-      namn_recieved: req.body.namn, // Jag fick ett namn, för jag requestade ett namn.
-      // efternamn_recieved: req.body.efternamn,
-      mobil_recieved: req.body.mobil,
-      email_recieved: req.body.email,
-      inlagg_recieved: req.body.inlagg,
-      pass_recieved: req.body.pass,
-   }]) // ALLT FUNKADE, ALLA VARIABLER STÄMMER!ja men..a
-});//<----- Ta en kringla vetja! Var inte blyg, det finns massor!a
+// AJAX-RELATERAT HÄR UNDER
+app.get("/andratextmedajax", (req,res) =>{
+   let asd = "Text från servern";
+   res.send(asd); 
+});;
+// AJAXRELATERAT HÄR OVANFÖR
 
+// app.post("/request", (req, res) => { 
+//    res.json([{
+//       namn_recieved: req.body.namn, 
+//       mobil_recieved: req.body.mobil,
+//       email_recieved: req.body.email,
+//       inlagg_recieved: req.body.inlagg,
+//       pass_recieved: req.body.pass,
+//    }]) 
+// });
+app.post("/request", (req,res) => {
+   const mottagetInlagg  = {
+            namn_recieved: req.body.namn, 
+            mobil_recieved: req.body.mobil,
+            email_recieved: req.body.email,
+            inlagg_recieved: req.body.inlagg,
+            pass_recieved: req.body.pass,
+         };
+             // Läser in data från .json-fil och sparar i JSON-format i variabel.
+    const filData = fs.readFileSync("inlagg.json");
+    omvandlaText = JSON.parse(filData);
+    omvandlaText.push(mottagetInlagg);
 
-// Jag är server, en kort klase med kod, jag lyssnar på port 3000 och svarar
-// på klienter (användarjäklarnas) anrop.
-// <----- det syns här till vänster att nodemon startar om servern varje gång jag skrivit
-// en endaste bokstav.
+    fs.writeFile("inlagg.json",JSON.stringify(omvandlaText, null, 2),
+      (err) => {
+        if (err) throw err;
+        console.log("Inlägget hanterades korrekt!");
+      });
+
 app.listen(port, () => {
    console.log(`server is running at ${port}`);
 });
-
+})
 /* 
 skriva till fil, läsa från fil
 */
@@ -74,5 +83,19 @@ skriva till fil, läsa från fil
        1.Användaren gör ett inlägg.
        2.Inlägget skickas till server.
        3.Server sparar den på befintlig fil och uppdaterar hemsidan utan att den laddas om.
+       
+       FRÅN MÖTE MED RONJA:
+       1. RONJA FIXAR SERVERVALIDERING
+       2. RONJA FIXAR LÄSA SKRIVA TILL FRÅN FIL json format etc.
+       3. MATTI KOLLAR UPP AJAX.
+       4. RONJA LADE TILL ADMIN PÅ PANG, NÄR VI SKRIVER IN ANV NAMN.
+       OCH LÖSENORD, KOLLAS DESSA MOT SPARAD INFO, VID KORREKT INFO:
+       SÅ SKA DET SOM SPARATS IN PÅ JSON FILEN, BOKNINGSFORMULÄRET
+       LÄSAS UPP PÅ HEMSIDAN, SERVER HÄMTAR FRÅN FILEN, LÄGGER UPP PÅ
+       SIDAN, MED AJAX. ALL BOKNINGSINFO FRÅN .JSONFIL VISAS FÖR ADMIN
+       PÅ SIDAN MED AJAX.
+       MATTI: Kolla upp PÅ ADMIN.HTML, kommunikation via ajax mellan:
+       DET SOM RONJA FIXAR, ÄR ATT SERVERN KAN LÄSA JSON OCH SKICKA TILL
+       ADMIN.HTML VIA KLIENTEN
       */
 ///// Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
--- publikmapp/klientmh.js
+++ publikmapp/klientmh.js
@@ -1,51 +1,80 @@
 // SÅHÄR BLEV DET FÄRDIGA RESULTATET:
-//var validator = require('validator');
 
+$(document).ready(function () { // samma som window.onload
+    $("#submit").click(function () { //obs! knappen i html har id="submit"
 
-
-$(document).ready(function () { 
-   
-    $("#submit").click(function () { 
-
-         // det går att ha let namn = doc.getbyID här sen namn i input i post.
          let namn = document.getElementById('namn').value;
-         // let efternamn = document.getElementById('efternamn').value;
          let mobil = document.getElementById('mobil').value;
          let email = document.getElementById('email').value;
-         let inlagg = document.getElementById('inlagg').value;
+         let inlagg = document.getElementById('inlagg').value; // samma för alla, fast de är olika typer = går via ID.
          let pass = document.getElementById('pass').value;
-         var namnLabel = document.getElementById('namnLabel');
-         let klartecken = false;
 
-         
-         var regexName = /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/i; // Christers specialbrunto!
+         var namnLabel = document.getElementById('namnLabel'); // hämtar ID för de som jag vill ändra Label - texten för.
+         var mobilLabel = document.getElementById('mobilLabel');
+         var emailLabel = document.getElementById('emailLabel');
+         var passLabel = document.getElementById('passLabel');
+
+         var regexName = /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/i; // namn
          
          let namnTest = function(reg,str) {
-            if (reg.test(str)) {
-               namnLabel.innerHTML="Namn OK!";
-               klartecken = true;
+            if (reg.test(str)) {          
+               namnLabel.innerHTML="Namn OK!"; 
+               return true; 
             }else{
-               namnLabel.innerHTML="Fel namninput! Försök igen";
-               
-               klartecken = false;
+               namnLabel.innerHTML="Fel namninput! Försök igen"; // fel-feedback kör jag just nu som texten ovanför inmatningsrutan!
+               return false; // obvious shit.
             }
          }
-      
-         // ovan hämtar alla inputs korrekt, nu ska validering göras på nåt vis under
-         // console.log(validator.isAlpha(namn,'sv-SE')); funkar bara utan mellanslag, tillägget options har jag inte koll på ännu..
-         namnTest(regexName,namn);
-         if(klartecken == true){
-         $.post("/request", 
+         let mobilTest = function(){ // Använder tilläggs-filen validator för att kolla om det är ett svenskt mobilnr/med/utan +46 och rätt längd.
+            if(validator.isMobilePhone(mobil,'sv-SE')){
+               mobilLabel.innerHTML = "Nr OK!";
+               return true;
+            }else{
+               mobilLabel.innerHTML = "Fel nummer! Kan du inte ditt egen nr eller är du bara sopslut?";
+               return false;
+            }
+         }
+         let emailTest = function(){
+            if(validator.isEmail(email)){ // validator-tillägget kollar om det är mail..
+               emailLabel.innerHTML="Mail OK!"; 
+               return true;
+            }else{
+               emailLabel.innerHTML="Fel format, försök igen!"; // diverse felmeddelanden för varje separat label
+               return false;
+            }
+         }
+         let passTest = function(){
+            if(validator.isStrongPassword(pass)){ // Den här är lite småkaxig, räknar ihop poäng eller bara true/false.
+               passLabel.innerHTML="Lösenord OK!"; // Default(för jag orkar inte ändra just nu..) är 8 tecken långt, 1 symbol, 1 siffra, 1 stor bokstav.
+               return true; ///////////////////////////////////////////////////// gröntext för att underlätta printscreen.. //////////////////////////////////////////////////////////
+            }else{
+               passLabel.innerHTML="Fel format på lösenord!";
+               return false;
+            }
+         }
+         let inlaggJustering = function(){ // Anti-ful -input. Gör om < till &;amp eller vafan det brukar va.
+             // Gör det med inlägg just nu, ska implementera överallt strax..
+            inlagg=validator.escape(inlagg);
+         }
+         // under: kollar att alla returnar = true, && för att varenda en ska stämma innan det skickas iväg
+         if(
+         namnTest(regexName,namn) == true && // Här får namnTest sina 2 inputs reg, str. dvs regexName vs namn jämförs.
+         mobilTest(mobil) == true &&
+         emailTest() == true &&
+         passTest() == true
+         )
+         {
+         inlaggJustering();// Jaha ja? nu stämmer allt, alla tester är true, men då kör vi justering av inläggets text med krokodiljanne mm...
+         $.post("/request", // och skickar iväg fanskapet till servern, hoppas det smakar!
           {
-             namn: namn,
-            //  efternamn: efternamn,
+             namn: namn, // div.krafs
              mobil: mobil,
              email: email,
              inlagg: inlagg,
              pass: pass
           },
-          function (data, status) {
-             console.log(data); 
+          function (data, status) { 
+             console.log(data); //
           });
 
           console.log("Namnvariabel: " + namn +
@@ -53,53 +82,48 @@ $(document).ready(function () {
           "\nEmailvariabel: "+ email + 
           "\nInläggvariabel: " + inlagg +
           "\nPasswordvariabel: " +pass);
-      }    
+          alert("Success!"); // jamen grattis
+          location.reload(); // laddar om hemsidan
+      }else{
+         event.preventDefault();
+         console.log("Något stämmer inte, bokningen har ej skickats.");
+      }
 });
+/*
+let datumTest = function () {
+      dt = new Date().toISOString().split(".")[0].replace(/[^\d]/gi, ""); // skapar datum utan symboler, bara siffror.
+      dagensdatum = dt.substring(0, 8); // endast ååååmmdd, inte mikrosekunder etc.
+      dagenstid = dt.substring(9, 12); // endast hhmm
+      formateradTid = tid.toString().split(".")[0].replace(":", ""); // tid utan krimskrams
+      formateratDatum = datum.toString().split(".")[0].replace(/[^\d]/gi, ""); //datum utan krimskrams
 
-// $(document).ready(function(){
+      if (formateratDatum >= dagensdatum) {
+        console.log("Datum OK!");
+        return true;
+      } else {
+        alert("Datum måste vara efter dagens datum");
+        console.log("Datum och Tid ERROR!");
+        return false;
+      }
+    };
+*/ // datumfunktionen jag gjorde i gruppuppg.
 
 
-// })
-// $(document).ready(function () { 
-   // namn: document.getElementById('namn').value,
-   // mobil: document.getElementById('mobil').value,
-   // email: document.getElementById('email').value,
-   // inlagg: document.getElementById('inlagg').value,
-   // pass: document.getElementById('pass').value,
-//    $("#submit").click(function () { 
 
+// AJAX-RELATERAT HÄR UNDER
+let hamtaData = function(){
+   let forfragan = new XMLHttpRequest(); // skapar nytt request.
 
-
-//        $.post("/request", 
-//           {
-//              namn: namn,
-//              mobil: mobil,
-//              email: email,
-//              inlagg: inlagg,
-//           },
-//           function (data, status) {
-//              console.log(data); 
-//           });
-
-//           console.log("Namnvariabel: " + namn +
-//           "\nMobilvariabel: "+ mobil + 
-//           "\nEmailvariabel: "+ email + 
-//           "\nInläggvariabel: " + inlagg +
-//           "\nPasswordvariabel: " +pass);
-//     });
-// });
-// DET HÄR KOPIERADE JAG FRÅN NÄTET:
-//  $(document).ready(function () {
-//     $("#submit").click(function () {
-//        $.post("/request",
-//           {
-//              name: "viSion",
-//              designation: "Professional gamer"
-//           },
-//           function (data, status) {
-//              console.log(data);
-//           });
-//     });
-//  });name: 
-// Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+   forfragan.open("GET", "/andratextmedajax"); // be servern om innehållet 
+   forfragan.onload = function(){ // det här startar när vi fått innehållet 
+      console.log("Servern svarade på min förfrågan med:");
+      console.log(this.response); // servern svarade med det här.
+      document.getElementById("ajaxText").innerHTML = this.response;
+   } // OVANFÖR: Ändrar texten under rubriken
+   forfragan.send();
+}
+$("#h2refresh").click(function () { // knapp anropar
+   hamtaData();
+});
 })
+// AJAX-RELATERAT HÄR OVANFÖR
