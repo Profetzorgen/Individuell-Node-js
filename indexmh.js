266a0363b8948a9354c5498c0c0ccfe66e439292
Profetzorgen <matti.heinonen1988@gmail.com>
2021-12-14 16:52:22 +0100

    pajade allt med en variabel, felsökte 6 h, ok nu

--- indexmh.js
+++ indexmh.js
@@ -8,31 +8,14 @@ const app = express();
 app.use(express.static(path.join(__dirname, "publikmapp")));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/hämtainläggen", (req,res) =>{
-  let inlaggUrFil = JSON.parse(fs.readFileSync("inlagga.json"));
-  let inlaggArray = {
-   datum: "",
-   namn: "",
-   inlagg: "",
-  };
-     for (let i = 0; i < inlaggUrFil.length; i++){
-      inlaggArray.datum = inlaggUrFil[i].datum_received,
-      inlaggArray.namn = inlaggUrFil[i].namn_received,
-      inlaggArray.inlagg = inlaggUrFil[i].inlagg_received,
-      JSON.stringify(inlaggArray, null, 2),(err) => {
-          if (err) throw err;
-          console.log("OK");
-        }
-      }
-      res.send(inlaggArray);
-});
+
 app.get("/", (req, res) =>{
   res.sendFile("indexmhh.html", {root: "./publikmapp"});
 });
 app.post("/inlagg", (req, res) =>{
   const inlagg = {
     datum_received: req.body.datum,
-    namn_received: req.body.namn,
+    nick_received: req.body.nick,
     inlagg_received: req.body.inlagg,
   }
   const inlaggData = fs.readFileSync("inlagga.json");
@@ -46,10 +29,11 @@ app.post("/inlagg", (req, res) =>{
       console.log("Inlägget lades till korrekt!");
     });
     // kombineraJson();
-    res.send()
+    
 });
 app.post("/createUser", (req,res) => { //fd. request i klientmh
-  const skapaAnv = {
+  const skapaAnv = { 
+    nick_received: req.body.nick,
     namn_received: req.body.namn,
     mobil_received: req.body.mobil,
     email_received: req.body.email,
@@ -65,43 +49,30 @@ app.post("/createUser", (req,res) => { //fd. request i klientmh
         if (err) throw err;
         console.log("Användaren skapades korrekt!");
       });
-      res.send();
 });
 // AJAX-RELATERAT, skickar ett enkelt svar om du trycker på en knapp på hemsidan.
 app.get("/andratextmedajax", (req,res) =>{
   let asd = "Servertext";
   res.send(asd);
 });
+app.get("/andratextmedajax2", (req,res) =>{
+  let asd = "Servertext";
+  res.send(asd);
+});
 app.post("/loggaIn", (req,res)=>{
-  let mottUppg = {
-    namn_received: req.body.namn,
+  let kontroll="Serversvar:";
+  const skapaAnv = { 
+    nick_received: req.body.nick,
     pass_received: req.body.pass,
-  };
-  let kontroll="";
-  // kolla om användaren existerar
-  let anvUrFil = JSON.parse(fs.readFileSync("skapaAnv.json"));
-  if(mottUppg.namn_received=="" || mottUppg.pass_received==""){
-    kontroll="FEL";
-  } else {
-    for(let i=0; i < anvUrFil.length; i++){
-      if(anvUrFil[i].namn_received == mottUppg.namn_received &&
-       anvUrFil[i].pass_received == mottUppg.pass_received){
-         kontroll = "OK";
-         console.log(kontroll);
-       }
-       else if(anvUrFil[i].namn_received != mottUppg.namn_received &&
-         anvUrFil[i].pass_received == mottUppg.pass_received){
-           kontroll = "namnfel";
-         }
-       else if(anvUrFil[i].namn_received == mottUppg.namn_received &&
-         anvUrFil[i].pass_received != mottUppg.pass_received){
-           kontroll = "passfel";
-       } else {
-         kontroll = "nullFel";
-       }
-  }
-  
- }
+  };   
+    const dataTagetUrFil = fs.readFileSync("skapaAnv.json");
+    anvArray = JSON.parse(dataTagetUrFil);
+    //anvArray.push(skapaAnv);
+    for(let i = 0; i < anvArray.length;i++){
+      if(skapaAnv.nick_received == anvArray[i].nick_received){
+        kontroll +="användaren finns";
+      } 
+    }
  res.send(kontroll);
  console.log(kontroll);
 });
--- publikmapp/klientmh.js
+++ publikmapp/klientmh.js
@@ -10,135 +10,114 @@ $(document).ready(function () { // samma som window.onload
    let cMonth = currentDate.getMonth() + 1;
    let cYear = currentDate.getFullYear();
    let datum =  cYear + "/" + cMonth + "/" + cDay +" - "+ time;
-   
-   
-   let inlaggSkickaRuta = document.getElementById("inlaggSkickaRuta");
-   let usersPanelen = document.getElementById("users");
    var onlineBool = false;
-   
+   var nick=""; //nick är odefinierat tills input per kodstycke.
+    let UppdateraRespons = function(){
+       let forfragan = new XMLHttpRequest(); // skapar nytt request.
+       forfragan.open("GET", "/andratextmedajax2"); // be servern om innehållet 
+       forfragan.onload = function(){ // det här startar när vi fått innehållet 
+       console.log("Ajax2:");
+       console.log(this.response); // servern svarade med det här.
+      $('#Respons').html(this.response);
+         
+       } 
+      forfragan.send();
+    }
    let hanteraOnline = function(vem){
       if(onlineBool === false){
-         usersPanelen.style.display = "block";
-         inlaggSkickaRuta.style.display = "none";
+         $('#users').show();
+         $('#inlaggSkickaRuta').hide();
+         //$('#Respons').html("Du måste vara online för att se eller göra inlägg");
          console.log("onlinebool: "+onlineBool + "\n  FRÅN:"+vem);
-         
+         console.log(namn);
       }
       else if (onlineBool===true){
-         usersPanelen.style.display = "none";
-         inlaggSkickaRuta.style.display = "block";
+         $('#users').hide();
+         $('#inlaggSkickaRuta').show();
          console.log("onlinebool: "+onlineBool +"\n  FRÅN: "+vem);
+         UppdateraRespons();
+         console.log(namn);
       }
    };
-   let namnTest = function(namn,namnLabel,regexName) {
+   hanteraOnline("Window.onload");
+   let namnTest = function(namn) {
+      let regexName = /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/i;
       if (regexName.test(namn)) {          
-         namnLabel.innerHTML="Namnformatet OK!"; 
-         console.log("Resultat nytt namntest: True");
+         $('#namnLabel').html("Namnet OK!");
+         console.log("Namnet OK!");
          return true; 
       }else{
-         namnLabel.innerHTML="Fel namninput! Försök igen"; 
-         console.log("Resultat nytt namntest: False");
-         return false; // obvious shit.
+         $('#namnLabel').html("Fel på namnet! För och Efternamn!");
+         console.log("Fel på namnet!");
+         return false; 
       }
    };
-   let passTest = function(pass,passLabel){
+   let passTest = function(pass){
       if(validator.isStrongPassword(pass)){ 
-         passLabel.innerHTML="Lösenordsformatet OK!"; 
-         console.log("Resultat nytt passtest: True");
+         $('#passLabel').html("Lösenord är OK!");
+         console.log("Lösenord är OK!");
          return true; 
       }else{
-         passLabel.innerHTML="Fel format på lösenord!";
-         console.log("Resultat nytt passtest: False");
+         $('#passLabel').html("Fel format på lösenord!");
+         console.log("Fel format på lösenord!");
          return false;
       }
    };
-   let mobilTest = function(mobil,mobilLabel){ 
+   let mobilTest = function(mobil){ 
       if(validator.isMobilePhone(mobil,'sv-SE')){
-         mobilLabel.innerHTML = "Nr OK!";
+         $('#mobilLabel').html("Mobilnr ok!");
+         console.log("Mobilnr ok!");
          return true;
       }else{
-         mobilLabel.innerHTML = "Fel nummer! Kan du inte ditt egen nr eller är du bara sopslut?";
+         $('#mobilLabel').html("Mobilnr är fel");
+         console.log("Mobilnr är fel");
          return false;
       }
    };
-   let emailTest = function(email,emailLabel){
+   let emailTest = function(email){
       if(validator.isEmail(email)){ 
-         emailLabel.innerHTML="Mail OK!"; 
+         $('#emailLabel').html("Mail OK!")
+         console.log("Email är OK!");
          return true;
       }else{
-         emailLabel.innerHTML="Fel format, försök igen!"; 
+         $('#emailLabel').html("Fel! har du @?");
+         console.log("Fel på email!");
          return false;
       }
    };
-   onlineBool = false;
-   hanteraOnline("Window.onload")
    $("#skickaUpp").click(function () { // skapar ny användare
-         
-         let namn = document.getElementById('namn').value;
-         let mobil = document.getElementById('mobil').value;
-         let email = document.getElementById('email').value;
-         let pass = document.getElementById('pass').value;
-
-         let mobilLabel = document.getElementById('mobilLabel');
-         let emailLabel = document.getElementById('emailLabel');
-         let namnLabel = document.getElementById('namnLabel');
-         let passLabel = document.getElementById('passLabel');
-         let regexName = /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/i;
-         if(namnTest(namn,namnLabel,regexName) == true &&
-         mobilTest(mobil,mobilLabel) == true &&
-         emailTest(email,emailLabel) == true &&
-         passTest(pass,passLabel) == true
-         )
-         {
+      nick=$('#nick').val(); // sätter den inloggade nick till den man skapade med
+      if(namnTest($('#namn').val()) == true &&
+         mobilTest($('#mobil').val()) == true &&
+         emailTest($('#email').val()) == true &&
+         passTest($('#pass').val()) == true){
          $.post("/createUser", 
           {
-             namn: namn,
-             mobil: mobil,
-             email: email,
-             pass: pass
+             nick: $('#nick').val(),
+             namn: $('#namn').val(), 
+             mobil: $('#mobil').val(),
+             email: $('#email').val(),
+             pass: $('#pass').val()
           },
           function (data, status) { 
-             console.log(data); 
+             $('#Respons').html(data); 
           });
           
-          alert("Grattis! kontot skapades! och du är online.");
-          onlineBool = true;
-          hanteraOnline("Registera ny användare");
-          
       }else{
-         
          console.log("Något stämmer inte. Har ej skickats.");
       }
-      
-      preventDefault();
-      
+      event.preventDefault();
 });
 
 $("#btnLogin").click(function () {
-   let namn = document.getElementById("loginUserName").value;
-   let pass = document.getElementById("loginPass").value;
-   
-   let namnLabel = document.getElementById('anvLabel2');
-   let passLabel = document.getElementById('passLabel2');
-   let regexName = /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/i;
-   let kontroll = "";
-   
-   if(namnTest(namn,namnLabel,regexName)==true &&
-   passTest(pass,passLabel)== true){
-      let respons = document.getElementById("Respons");
+   nick = $('#loginNick'); // inloggningsrutan för nick
+   if(passTest($("#loginPass").val())== true){
       $.post("/loggaIn",{
-         namn: namn,
-         pass: pass
+         nick: $('#loginNick').val(), // från logga in befintlig
+         pass: $("#loginPass").val()
          },
          function (data, status) {
-            respons.innerHTML = data,
             console.log(data);
-            if(data==("OK")){
-               onlineBool=true;
-               hanteraOnline("Logga in befintlig användare");
-            }else{
-               onlineBool=false;
-               hanteraOnline("Logga in befintlig användare");
-            }
          });
    }else{
       alert("något valideringsfel!");
@@ -146,76 +125,57 @@ $("#btnLogin").click(function () {
    event.preventDefault();
 });
 $("#inlaggSkickaKnapp").click(function () {
-   let namn = document.getElementById("loginUserName").value;
-   let inlaggSkicka = document.getElementById("inlaggSkicka").value;
-   if(inlaggSkicka.value!==null){
+   
+   $('#h3text').html("inlägg skickat!");
+   if($('#inlaggSkicka').val()!==""){
       $.post("/inlagg",{
          datum: datum,
-         namn: namn,
-         inlagg: inlaggSkicka
+         nick: nick, // var-variabeln valdes vid inloggningsmetoden.
+         inlagg: $('#inlaggSkicka').val()
          },
          function (data, status) {
             console.log(data);
+            
          });
    }else{
-      alert("Något gick snett");
+      $("#h3text").html("Skriv något");
    }
    event.preventDefault();
+   UppdateraRespons();
 });
 LOGGAUT = function(){
    if(onlineBool!==false){
       onlineBool = false;
       hanteraOnline("Logga Ut Knappen");
       alert("du loggas nu ut.");
+      location.reload();
+      
    }else{
       alert("du är redan utloggad!");
    }
 }
 loggaKnapp = function () {
-
-   var loggaAnvForm = document.getElementById("loggaAnvForm");
-   var skapAnvForm = document.getElementById("skapAnvForm");
-   if (!loggaAnvForm.style.display || loggaAnvForm.style.display==="none"){
-      loggaAnvForm.style.display ="block";
-      skapAnvForm.style.display = "none"
-   } else {
-      loggaAnvForm.style.display = "none";
-   }
+   $('#loggaAnvForm').show();
+   $('#skapaAnvForm').hide();
 };
 skapaKnapp = function () {
-   var skapAnvForm = document.getElementById("skapAnvForm");
-   var loggaAnvForm = document.getElementById("loggaAnvForm");
-   if (!skapAnvForm.style.display || skapAnvForm.style.display==="none"){
-      skapAnvForm.style.display ="block";
-      loggaAnvForm.style.display = "none"
-   } else {
-      skapAnvForm.style.display = "none";
-   }
+   $('#skapaAnvForm').show();
+   $('#loggaAnvForm').hide();
 };
 // AJAX-RELATERAT HÄR UNDER
 let hamtaData = function(){
    let forfragan = new XMLHttpRequest(); // skapar nytt request.
    forfragan.open("GET", "/andratextmedajax"); // be servern om innehållet 
    forfragan.onload = function(){ // det här startar när vi fått innehållet 
-      console.log("Servern svarade på min förfrågan med:");
+      console.log("Ajax1:");
       console.log(this.response); // servern svarade med det här.
-      document.getElementById("ajaxText").innerHTML = this.response;
+      $("#ajaxText").html(this.response);
    } // OVANFÖR: Ändrar texten under rubriken
    forfragan.send();
-}
-let hamtaInlagg = function() {
-   if(onlineBool===true){
-      let forfragan = new XMLHttpRequest();
-      forfragan.open("GET","/hämtainläggen");
-      forfragan.onload = function () {
-         document.getElementById("Respons").innerHTML = this.response;
-      }
-   }forfragan.send();
 }
 $("#h2refresh").click(function () { // knapp anropar
    hamtaData();
    hanteraOnline("Uppdateraknappen");
-   hamtaInlagg();
 });
 });
 // AJAX-RELATERAT HÄR OVANFÖR
