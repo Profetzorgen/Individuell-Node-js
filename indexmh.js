4af31052a474ba9a134ef8996753dc3b6d46ee7f
Profetzorgen <matti.heinonen1988@gmail.com>
2021-12-14 19:52:12 +0100

    Gröntext klart

--- indexmh.js
+++ indexmh.js
@@ -1,27 +1,25 @@
-const express = require("express");
-const fs = require("fs");
+const express = require("express"); // Diverse prylar servern behöver, några har jag lagt till
+const fs = require("fs");// Och några dök upp av sig själv när jag klantade mig.
 const path = require("path"); 
 const { body, validationResult } = require("express-validator");
-const M = require("minimatch");
-const PORT = process.env.PORT || 3000;
-const app = express();
+const M = require("minimatch"); // den här kom ur tomma intet.. vågar inte röra något.
+const PORT = process.env.PORT || 3000; // port
+const app = express(); // servern använder express
 app.use(express.static(path.join(__dirname, "publikmapp")));
-app.use(express.urlencoded({ extended: false }));
-
-
+app.use(express.urlencoded({ extended: false }));  // express är med flit
 app.get("/", (req, res) =>{
-  res.sendFile("indexmhh.html", {root: "./publikmapp"});
+  res.sendFile("indexmhh.html", {root: "./publikmapp"}); // tillgång till filer
 });
-app.post("/inlagg", (req, res) =>{
-  const inlagg = {
-    datum_received: req.body.datum,
-    nick_received: req.body.nick,
-    inlagg_received: req.body.inlagg,
+app.post("/inlagg", (req, res) =>{ // Hanterar inlägg, hela denna råkade jag radera cleant.
+  const inlagg = { // felsökte och felsökte..
+    datum_received: req.body.datum, // försökte först köra med kontroll av att användaren redan inte finns..
+    nick_received: req.body.nick, // och olika knep men nick OCH namn = fungerar.
+    inlagg_received: req.body.inlagg, 
   }
-  const inlaggData = fs.readFileSync("inlagga.json");
+  const inlaggData = fs.readFileSync("inlagga.json"); // läser ur inläggsregistret
   omvandlaInlagg = JSON.parse(inlaggData);
-  omvandlaInlagg.push(inlagg);
-  fs.writeFileSync(
+  omvandlaInlagg.push(inlagg);  // formaterar till array och slutligen 
+  fs.writeFileSync( // skriver den till registret igen med nytt info.
     "inlagga.json",
     JSON.stringify(omvandlaInlagg, null, 2),
     (err) => {
@@ -31,16 +29,16 @@ app.post("/inlagg", (req, res) =>{
     res.send()
 });
 app.post("/createUser", (req,res) => { //fd. request i klientmh
-  const skapaAnv = { 
+  const skapaAnv = { // denna innehåller beståndsdelarna till en fullständig användare.
     nick_received: req.body.nick,
     namn_received: req.body.namn,
     mobil_received: req.body.mobil,
     email_received: req.body.email,
     pass_received: req.body.pass,
   };   
-    const dataTagetUrFil = fs.readFileSync("skapaAnv.json");
+    const dataTagetUrFil = fs.readFileSync("skapaAnv.json"); // läser in befintliga användare.
     anvArray = JSON.parse(dataTagetUrFil);
-    anvArray.push(skapaAnv);
+    anvArray.push(skapaAnv); // formaterar och sparar.
     fs.writeFile(
       "skapaAnv.json",
       JSON.stringify(anvArray, null, 2),
@@ -48,46 +46,46 @@ app.post("/createUser", (req,res) => { //fd. request i klientmh
         if (err) throw err;
         console.log("Användaren skapades korrekt!");
       });
-      res.send("1");
-});
+      res.send("1");// skickar en 1a i strängform
+});                 // för att kunna === jämföra och bekräfta: användare online i klientskriptet
 // AJAX-RELATERAT, skickar ett enkelt svar om du trycker på en knapp på hemsidan.
 app.get("/andratextmedajax", (req,res) =>{
   let asd = "Servertext";
   res.send(asd);
 });
-// den här skickar nu inlägg vid begäran!
+// den här skickar nu inlägg vid begäran! vidareutveckling av ovanstående.
 app.get("/andratextmedajax2", (req,res) =>{
   const inl = JSON.parse(fs.readFileSync("inlagga.json"));
   let inlTextForm = "";
   for (let i = 0; i < inl.length; i++) {
     inlTextForm +=
       "Datum: "+inl[i].datum_received+"<br>"+
-      "Nick: "+inl[i].nick_received +"<br>"+
+      "Nick: "+inl[i].nick_received +"<br>"+ // samma som gemensamma projektet, fast light.
       "Inlägg: "+inl[i].inlagg_received+"<br><br>" ;
   }
-  res.send(inlTextForm);
+  res.send(inlTextForm); // skickar det omvandlade registret (plus ev nytt inlägg!) på begäran
 });
 app.post("/loggaIn", (req,res)=>{
   let svar="";
-  const skapaAnv = { 
+  const skapaAnv = { // för att logga in behöver man bara nick och password.
     nick_received: req.body.nick,
     pass_received: req.body.pass,
-  };   console.log("Tog emot NICK: "+skapaAnv.nick_received +
+  };   console.log("Tog emot NICK: "+skapaAnv.nick_received + // visar för serverkonsollen ikommande inloggningsuppgifter.
   "\nPASS: "+skapaAnv.pass_received);
     const dataTagetUrFil = fs.readFileSync("skapaAnv.json");
     anvArray = JSON.parse(dataTagetUrFil);
-    //anvArray.push(skapaAnv);
+    //anvArray.push(skapaAnv); behövs inte för att kontrollera användare med for-loop.
 
     for(let i = 0; i < anvArray.length;i++){
       if(skapaAnv.nick_received == anvArray[i].nick_received &&
         skapaAnv.pass_received == anvArray[i].pass_received){
-        svar ="1";
+        svar ="1"; // "1" som sträng sätter användare som online i klientskriptets ( två ställen.)
       } else if (skapaAnv.nick_received == anvArray[i].nick_received &&
         skapaAnv.pass_received != anvArray[i].pass_received){
-          svar ="2";
+          svar ="2"; // "2" är nåt annat än "1"
         } 
     }
- res.send(svar);
+ res.send(svar); // skickar 1:an eller 2:an
  console.log(svar);
 });
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- publikmapp/klientmh.js
+++ publikmapp/klientmh.js
@@ -1,45 +1,41 @@
 $(document).ready(function () { // samma som window.onload
-   let currentDate = new Date();
+   let currentDate = new Date(); // skapar och formaterar tid och datum till läsligt format.
    let time = currentDate.getHours() + ":" + currentDate.getMinutes();
    let cDay = currentDate.getDate();
-   let cMonth = currentDate.getMonth() + 1;
+   let cMonth = currentDate.getMonth() + 1; // för att inga nollor ska försvinna i den binära etern.
    let cYear = currentDate.getFullYear();
-   let datum =  cYear + "/" + cMonth + "/" + cDay +" - "+ time;
+   let datum =  cYear + "/" + cMonth + "/" + cDay +" - "+ time; // tid,datum som sträng.
    var onlineBool = false;
-   var nick=""; //nick är odefinierat tills input per kodstycke.
-    let UppdateraRespons = function(){
-       let forfragan = new XMLHttpRequest(); // skapar nytt request.
-       forfragan.open("GET", "/andratextmedajax2"); // be servern om innehållet 
-       forfragan.onload = function(){ // det här startar när vi fått innehållet 
-       console.log("Ajax2:");
-       console.log(this.response); // servern svarade med det här.
-      $('#Respons').html(this.response);
-         
-       } 
-      forfragan.send();
-    }
-   let hanteraOnline = function(vem){
-      if(onlineBool === false){
-         $('#users').show();
+   var nick=""; // nick är odefinierat tills input per kodstycke.
+   let UppdateraRespons = function(){ // Denna ajaxar fram inläggen när det behövs uppdateras.
+      let forfragan = new XMLHttpRequest(); // skapar nytt request.
+      forfragan.open("GET", "/andratextmedajax2"); // be servern om innehållet 
+      forfragan.onload = function(){ // det här startar när vi fått innehållet 
+      console.log("Ajax2:");
+      console.log(this.response); // all radda med text i console beror på den här.
+      $('#Respons').html(this.response); // och blir såhär när det skrivs ut.
+      } // det skrivs på en Label, finns garanterat smidigare sätt att göra men nu vill jag ha in uppgiften.
+      forfragan.send();// ovanstående förberedelser klara, skicka förfrågan om innehållet.
+   }
+   let hanteraOnline = function(vem){ // Inloggad eller inte? "vem" är för att se i konsollen VAR bool blev False;
+      if(onlineBool === false){ //denna hanterar vad som visas av:
+         $('#users').show(); // "logga in"/"skapa användare"/"gör ett inlägg"
          $('#inlaggSkickaRuta').hide();
-         
          $('#Respons').html("Du måste vara online för att se eller göra inlägg");
          console.log("onlinebool: "+onlineBool + "\n  FRÅN:"+vem);
-      
       }
       else if (onlineBool===true){
-         $('#users').hide();
+         $('#users').hide(); // om man är inloggad behöver man inte logga in etc.
          $('#inlaggSkickaRuta').show();
          console.log("onlinebool: "+onlineBool +"\n  FRÅN: "+vem);
-         UppdateraRespons();
-         
+         UppdateraRespons(); // hämta mer inlägg om det finns.
       }
    };
-   hanteraOnline("Window.onload");
-   let namnTest = function(namn) {
+   hanteraOnline("Window.onload"); // inloggad=False, syns i konsollen
+   let namnTest = function(namn) { // namntest, som det låter.
       let regexName = /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/i;
       if (regexName.test(namn)) {          
-         $('#namnLabel').html("Namnet OK!");
+         $('#namnLabel').html("Namnet OK!"); // "skapa ny användare" har tester, logga in har det inte!
          console.log("Namnet OK!");
          return true; 
       }else{
@@ -48,7 +44,7 @@ $(document).ready(function () { // samma som window.onload
          return false; 
       }
    };
-   let passTest = function(pass){
+   let passTest = function(pass){ // testar om det är ett starkt lösen, behövs inte för de som redan har inloggningar.
       if(validator.isStrongPassword(pass)){ 
          $('#passLabel').html("Lösenord är OK!");
          console.log("Lösenord är OK!");
@@ -59,8 +55,8 @@ $(document).ready(function () { // samma som window.onload
          return false;
       }
    };
-   let mobilTest = function(mobil){ 
-      if(validator.isMobilePhone(mobil,'sv-SE')){
+   let mobilTest = function(mobil){ // alla tester både här och på gemensamma i klientskript är jag skyldig till..
+      if(validator.isMobilePhone(mobil,'sv-SE')){ // därav den något krassliga designen.
          $('#mobilLabel').html("Mobilnr ok!");
          console.log("Mobilnr ok!");
          return true;
@@ -72,7 +68,7 @@ $(document).ready(function () { // samma som window.onload
    };
    let emailTest = function(email){
       if(validator.isEmail(email)){ 
-         $('#emailLabel').html("Mail OK!")
+         $('#emailLabel').html("Mail OK!") // som sagt, inga tester på logga in befintlig.
          console.log("Email är OK!");
          return true;
       }else{
@@ -82,31 +78,31 @@ $(document).ready(function () { // samma som window.onload
       }
    };
    $("#skickaUpp").click(function () { // skapar ny användare
-      nick=$('#nick').val(); // sätter den inloggade nick till den man skapade med
-      if(namnTest($('#namn').val()) == true &&
+      nick=$('#nick').val(); // sätter den inloggades nick till detta
+      if(namnTest($('#namn').val()) == true && // valideringar
          mobilTest($('#mobil').val()) == true &&
          emailTest($('#email').val()) == true &&
          passTest($('#pass').val()) == true){
-         $.post("/createUser", 
+         $.post("/createUser", // anropa routen på indexmh.js
           {
              nick: $('#nick').val(),
              namn: $('#namn').val(), 
-             mobil: $('#mobil').val(),
-             email: $('#email').val(),
+             mobil: $('#mobil').val(), // skicka värdena ur formuläret, jQuery style.
+             email: $('#email').val(), // slapp xAntal meter kod med detta ist för document.GetElementByID.......
              pass: $('#pass').val()
           },
           function (data, status) { 
-            if(data==="1"){
+            if(data==="1"){ // Här bekräftas att användaren är online, === jämförelse, sträng och etta.
                onlineBool = true;
                hanteraOnline("Från user create");
-               // ? UppdateraRespons();
-            } else if (data==="2"){
+               // ? UppdateraRespons(); överflödig här
+            } else if (data==="2"){ // servern säger = fel användarnamn/lösen via sträng-siffror.
                onlineBool=false;
                hanteraOnline("Från user create");
             }
           });
       }else{
-         console.log("Något stämmer inte. Har ej skickats.");
+         console.log("Något stämmer inte. Har ej skickats."); // när man rotar i koden är extra livrem bra att ha.
       }
       event.preventDefault();
 });
@@ -136,21 +132,20 @@ $("#btnLogin").click(function () {
       alert("något valideringsfel!");
    }
   
-   
    event.preventDefault();
 });
-$("#inlaggSkickaKnapp").click(function () {
+$("#inlaggSkickaKnapp").click(function () { // id. för skicka inlägget knappen.
    
-   $('#h3text').html("inlägg skickat!");
+   $('#h3text').html("inlägg skickat!"); // visar tydligt.
    if($('#inlaggSkicka')){
       $.post("/inlagg",{
          datum: datum,
          nick: nick,
-         inlagg: $('#inlaggSkicka').val()
+         inlagg: $('#inlaggSkicka').val() // jquery kortade ner koden en aning! ändå alldeles för lång men får duga.
          },
          function (data, status) {
             console.log(data);
-            UppdateraRespons();
+            UppdateraRespons(); // att inlägget är skicka,
          });
          
    }else{
@@ -159,18 +154,18 @@ $("#inlaggSkickaKnapp").click(function () {
    event.preventDefault();
 
 });
-LOGGAUT = function(){
+LOGGAUT = function(){ // anropas av själva knappen, i html.
    if(onlineBool!==false){
       onlineBool = false;
-      hanteraOnline("Logga Ut Knappen");
-      alert("du loggas nu ut.");
-      location.reload();
+      hanteraOnline("Logga Ut Knappen"); // sätter onlineBool som false och berättar varifrån
+      alert("du loggas nu ut."); // lite mysig pop-up.
+      location.reload(); // ladda om sidan, det närmaste jag associerar med console.Clear() i c#
       
    }else{
       alert("du är redan utloggad!");
    }
 }
-loggaKnapp = function () {
+loggaKnapp = function () { // gömmer/visar knappar så inte alla visas samtidigt etc. ej perfekt kod!
    $('#loggaAnvForm').show();
    $('#skapaAnvForm').hide();
 };
@@ -178,7 +173,7 @@ skapaKnapp = function () {
    $('#skapaAnvForm').show();
    $('#loggaAnvForm').hide();
 };
-// AJAX-RELATERAT HÄR UNDER
+// AJAX-RELATERAT HÄR UNDER, den Holger visade.
 let hamtaData = function(){
    let forfragan = new XMLHttpRequest(); // skapar nytt request.
    forfragan.open("GET", "/andratextmedajax"); // be servern om innehållet 
@@ -189,7 +184,7 @@ let hamtaData = function(){
    } // OVANFÖR: Ändrar texten under rubriken
    forfragan.send();
 }
-$("#h2refresh").click(function () { // knapp anropar
+$("#h2refresh").click(function () { // Testknappen för ajax.
    hamtaData();
    hanteraOnline("Uppdateraknappen");
 
